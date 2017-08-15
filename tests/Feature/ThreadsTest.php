2f4682178fc5dbdd41679f73210965dc309fc816
Slawa Gladkov <slawa.gladkov@invision.de>
2017-08-15 09:06:57 +0200

    Add @index @show to ThreadController along with views

---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function threads_are_accessible()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function a_user_can_browse_threads()
+    {
+        $thread = factory('App\Thread')->create();
+        $response = $this->get('/threads');
+
+        $response->assertSee($thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_browse_particular_thread()
+    {
+        $thread = factory('App\Thread')->create();
+        $response = $this->get('/threads/'.$thread->id);
+
+        $response->assertSee($thread->title);
+        $response->assertSee($thread->body);
+    }
+}
