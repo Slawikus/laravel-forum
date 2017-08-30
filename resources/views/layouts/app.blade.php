32802f96e076989c586d9ff884452f2b0fca7a58
Slawa Gladkov <slawa.gladkov@invision.de>
2017-08-30 18:46:25 +0200

    Change profile and thread views, and link to user profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,6 +79,9 @@
 
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
+                                        <a href="{{ route('profile', Auth::user() )}}">My profile</a>
+                                    </li>
+                                    <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
