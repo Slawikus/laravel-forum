1bad97042f829175693c29539accadd4e6175ab7
Slawa Gladkov <slawa.gladkov@invision.de>
2017-09-15 18:31:59 +0200

    Delete favourites activities after delete the reply

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -10,12 +10,14 @@
                 <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
             </h1>
         </div>
-        @foreach($activities as $date => $activity)
+        @forelse($activities as $date => $activity)
             <h3 class="page-header">{{ $date }}</h3>
             @foreach($activity as $record)
                 @include("profiles.activities.{$record->type}", ['activity' => $record])
             @endforeach
-        @endforeach
+        @empty
+            <p>There is no activity for this user yet.</p>
+        @endforelse
 
         {{-- {{ $threads->links() }} --}}
         </div>
